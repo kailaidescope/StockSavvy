dc6febe86878059688f4bc216315515ab28dcdc1
GitExcited <josedruiz1@gmail.com>
2025-02-01 17:43:45 -0500

    merge

--- Sunlife/src/App.jsx
+++ Sunlife/src/App.jsx
@@ -3,16 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import InvestmentDashboard from './components/investment-dashboard';
 import Search from './components/search-page';
 import AdvancedSearch from './components/advanced-search-page';
-
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<InvestmentDashboard />} />
-        <Route path="/search/:stock" element={<Search />} />
-        <Route path="/advanced-search" element= {<AdvancedSearch/>}/>
-      </Routes>
-    </Router>
 import { SymbolProvider } from './contexts/symbol-context';
 
 const App = () => {
@@ -22,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<InvestmentDashboard />} />
           <Route path="/search/:stock" element={<Search />} />
+          <Route path="/advanced-search" element= {<AdvancedSearch/>}/>
         </Routes>
       </Router>
     </SymbolProvider> 
